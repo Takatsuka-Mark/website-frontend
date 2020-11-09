0048e3b66774a7a475e95e93863b1dcb409d1d76
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-11-08 23:14:49 -0800

    Updated Overview page. Deployed.

--- src/pages/AboutMe/Overview/Overview.vue
+++ src/pages/AboutMe/Overview/Overview.vue
@@ -7,70 +7,66 @@
     <h1 class="page-title">
       Current Status - <span class="fw-semi-bold">Working ʕ•́ᴥ•̀ʔっ</span>
     </h1>
-    <b-row>
-      <!--      <b-col lg="7">-->
-      <b-col>
-        <!--        <Widget title="<h2>Timeline</h2>" custom-header class="bg-transparent">-->
-        <!--          <AboutMeTimeline/>-->
-        <!--        </Widget>-->
-        <Widget title="<h2><b>Rochester Institute of Technology</b></h2>"
-                custom-header collapse class="bg-transparent">
-          <h4><b>Bachelors Computer Science</b></h4>
-          <h4>Aug. 2018 - Dec. 2021</h4>
-          <h5>Minor in Mathematics</h5>
-          <h5><b>GPA: 3.86</b></h5>
-        </Widget>
-        <Widget title="<h2><b>SecureCloudDB</b></h2>"
-                custom-header collapse class="bg-transparent">
-          <h4><b>Software Engineering Intern</b></h4>
-          <h4>May 2020 - Current</h4>
-          <h5>Backend Developer</h5>
-          <!--          <h5>Skills Acquired</h5>-->
-
-        </Widget>
-        <b-container>
-          <b-row>
+    <b-col>
+      <!--        <Widget title="<h2>Timeline</h2>" custom-header class="bg-transparent">-->
+      <!--          <AboutMeTimeline/>-->
+      <!--        </Widget>-->
+      <Widget title="<h2><b>Rochester Institute of Technology</b></h2>"
+              custom-header class="bg-transparent">
+        <h4><b>Bachelors Computer Science</b></h4>
+        <h4>Aug. 2018 - Dec. 2021</h4>
+        <h5>Minor in Mathematics</h5>
+        <h5><b>GPA: 3.86</b></h5>
+      </Widget>
+      <Widget title="<h2><b>SecureCloudDB</b></h2>"
+              custom-header class="bg-transparent">
+        <h4><b>Software Engineering Intern</b></h4>
+        <h4>May 2020 - Current</h4>
+        <h5>Backend Developer</h5>
+      </Widget>
+      <b-container>
+        <b-row>
+          <b-col>
             <Widget title="<h2><b>Programming Languages</b></h2>"
-                    custom-header collapse class="bg-transparent">
+                    custom-header class="bg-transparent">
               <h5>Experience in Months</h5>
               <language-gauge/>
             </Widget>
+          </b-col>
+          <b-col>
             <Widget title="<h2><b>Software Skills</b></h2>" custom-header
-                    collapse class="bg-transparent">
+                    class="bg-transparent">
               <b-container>
-                <b-row>
-                    <b-col sm="4">
-                      <b-img src="Logos/AWS.png" height="30px" style="margin-right: 10px"/>
-                      <b-link href="https://aws.amazon.com/" target="_blank"><h3>Amazon Web Services</h3></b-link>
-                    </b-col>
-                  <b-col sm="4">
-                    <b-img src="Logos/Google.png" height="30px" style="margin-right: 10px"/>
-                    <b-link href="https://cloud.google.com/" target="_blank"><h3>Google Cloud</h3></b-link>
-                  </b-col>
-                  <b-col sm="4">
-                    <b-img src="Logos/Kubernetes.png" height="30px" style="margin-right: 10px"/>
-                    <b-link href="https://kubernetes.io/" target="_blank"><h3>Kubernetes</h3></b-link>
-                  </b-col>
-                  <div class="w-100"></div>
-                  <b-col sm="4">
-                    <b-img src="Logos/psql.png" height="30px" style="margin-right: 10px"/>
-                    <b-link href="https://postgresql.org/" target="_blank"><h3>PostgreSQL</h3></b-link>
-                  </b-col>
-                  <b-col sm="4">
-                    <b-img src="Logos/Tensorflow.png" height="30px" style="margin-right: 10px"/>
-                    <b-link href="https://tensorflow.org/" target="_blank"><h3>Tensorflow</h3></b-link>
-                  </b-col>
-                  <b-col sm="4">
-                    <b-img src="Logos/Spring.png" height="30px" style="margin-right: 10px"/>
-                    <b-link href="https://spring.io/" target="_blank"><h3>Spring</h3></b-link>
-                  </b-col>
-                </b-row>
+                <b-col sm="4">
+                  <b-img src="Logos/AWS.png" height="30px" style="margin-right: 10px"/>
+                  <b-link href="https://aws.amazon.com/" target="_blank"><h3>Amazon Web Services</h3></b-link>
+                </b-col>
+                <b-col sm="4">
+                  <b-img src="Logos/Google.png" height="30px" style="margin-right: 10px"/>
+                  <b-link href="https://cloud.google.com/" target="_blank"><h3>Google Cloud</h3></b-link>
+                </b-col>
+                <b-col sm="4">
+                  <b-img src="Logos/Kubernetes.png" height="30px" style="margin-right: 10px"/>
+                  <b-link href="https://kubernetes.io/" target="_blank"><h3>Kubernetes</h3></b-link>
+                </b-col>
+                <b-col sm="4">
+                  <b-img src="Logos/psql.png" height="30px" style="margin-right: 10px"/>
+                  <b-link href="https://postgresql.org/" target="_blank"><h3>PostgreSQL</h3></b-link>
+                </b-col>
+                <b-col sm="4">
+                  <b-img src="Logos/Tensorflow.png" height="30px" style="margin-right: 10px"/>
+                  <b-link href="https://tensorflow.org/" target="_blank"><h3>Tensorflow</h3></b-link>
+                </b-col>
+                <b-col sm="4">
+                  <b-img src="Logos/Spring.png" height="30px" style="margin-right: 10px"/>
+                  <b-link href="https://spring.io/" target="_blank"><h3>Spring</h3></b-link>
+                </b-col>
               </b-container>
             </Widget>
-          </b-row>
-        </b-container>
-      </b-col>
-    </b-row>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-col>
   </div>
 </template>
 
