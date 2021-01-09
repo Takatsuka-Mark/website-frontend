83a124951de086d74493e052bdffc92ea046d041
Takatsuka-mark <takatsuka.mark@gmail.com>
2021-01-08 17:23:24 -0800

    Add Precision Scale

--- src/pages/Projects/MathInterpreter/MathInterpreter.vue
+++ src/pages/Projects/MathInterpreter/MathInterpreter.vue
@@ -10,11 +10,21 @@
     <div class="expressionInput">
       <b-input id="expression-input" size="lg" placeholder="Enter your expression here"
                v-model="inputExpression" v-on:keyup.enter="getResult"/>
+      <b-row>
+        <b-col>
+      <span>Display Precision: {{ displayPrecision }}
+      </span>
+        </b-col>
+        <b-col>
+          <b-input style="display:inline-block" v-model="displayPrecision"
+                   id="precisionRange"
+                   type="range" min="1" max="100"/>
+        </b-col>
+      </b-row>
     </div>
     <div style="padding: 5%">
       <h2>{{ result }}</h2>
-      <div class="expressionOutput">
-      </div>
+      <div class="expressionOutput"/>
     </div>
   </div>
 </template>
@@ -28,6 +38,7 @@ export default {
   data: () => ({
     inputExpression: '',
     result: 'Results shown here!',
+    displayPrecision: '10',
   }),
   methods: {
     getResult() {
@@ -36,7 +47,7 @@ export default {
 
       get(`http://35.185.254.123:8085/math/evaluate?${searchParams.toString()}`)
         // eslint-disable-next-line no-return-assign
-        .then((response) => (this.result = response));
+        .then((response) => (this.result = response.data));
     },
   },
 };
