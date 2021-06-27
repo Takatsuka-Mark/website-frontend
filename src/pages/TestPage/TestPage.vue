20dd0677c27346b4f6b46eb4d8e18488b71b43a3
Takatsuka-mark <takatsuka.mark@gmail.com>
2021-06-27 16:23:13 -0400

    Created simple containerization. Added test-page. Testing stacking blocks.

---
+++ src/pages/TestPage/TestPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Container>
+      <div class="wrapper">
+        <div class="title-block">
+          Hi, I'm Mark :D
+        </div>
+        <div class="text-block">
+          Welcome to my experimental website!
+        </div>
+      </div>
+    </Container>
+  </div>
+</template>
+
+<script>
+import Container from '@/components/Container/Container.vue';
+
+export default {
+  name: 'TestPage.vue',
+  components: { Container },
+};
+</script>
+
+<style scoped>
+
+.wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.title-block .text-block {
+  width: 50%
+}
+
+@media screen and (min-width: 900px) {
+  .title-block .text-block {
+    width: 100%
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .wrapper {
+    display: block;
+  }
+}
+.text-block{
+
+}
+</style>
