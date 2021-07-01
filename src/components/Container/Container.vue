0aa06523328a4908c5d8d1e4cc8880831abaf8e6
Takatsuka-mark <takatsuka.mark@gmail.com>
2021-06-30 23:28:01 -0400

    Added vertical container listing

--- src/components/Container/Container.vue
+++ src/components/Container/Container.vue
@@ -1,20 +1,19 @@
 <template>
-  <section ref="container">
-    <div class="container-contents">
-      <h5 v-if="title && typeof title === 'string'" class="title">{{title}}</h5>
-      <!-- Horizontal Double Box -->
-      <div v-if="horizontal_title" class="ht_wrapper">
-        <div class="ht_title" v-if="horizontal_title_left">
-          <slot name="ht_title"></slot>
-        </div>
-        <div class="ht_body">
-          <slot class="ht_body" name="ht_body"></slot>
-        </div>
-        <div class="ht_title" v-if="!horizontal_title_left">
-          <slot name="ht_title"></slot>
-        </div>
+  <section class="container">
+    <h5 v-if="title && typeof title === 'string'" class="title">{{title}}</h5>
+    <!-- Horizontal Double Box -->
+    <div v-if="horizontal_title" class="ht_wrapper">
+      <div class="ht_title" v-if="horizontal_title_left">
+        <slot name="ht_title"></slot>
+      </div>
+      <div class="ht_body">
+        <slot class="ht_body" name="ht_body"></slot>
+      </div>
+      <div class="ht_title" v-if="!horizontal_title_left">
+        <slot name="ht_title"></slot>
       </div>
     </div>
+    <slot></slot>
   </section>
 </template>
 
--- src/pages/TestPage/TestPage.vue
+++ src/pages/TestPage/TestPage.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <Container horizontal_title="true">
-      <template slot="ht_title">
-        Hi, I'm Mark <span style="width: 2rem"/> (^o^)/
-      </template>
-      <template slot="ht_body">
-        Welcome to my experimental website!
-        this is a lot of text that I am just
-        putting in here so that it gets filled with even more text
-      </template>
-    </Container>
+    <div class="center_column">
+      <Container class="intro_container" horizontal_title="true">
+        <template slot="ht_title">
+          Hi, I'm Mark <span style="width: 2rem"/> (^o^)/
+        </template>
+        <template slot="ht_body">
+          Welcome to my experimental website!
+          this is a lot of text that I am just
+          putting in here so that it gets filled with even more text
+        </template>
+      </Container>
+      <Container class="education_container" horizontal_title="true" horizontal_title_left="false">
+        <template slot="ht_title">
+          Education
+        </template>
+        <template slot="ht_body">
+          This is a ton of information about education that I'm just so willing to pass around
+          with absolutely no consideration of life at all
+        </template>
+      </Container>
+    </div>
   </div>
 </template>
 
@@ -22,5 +33,16 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.center_column {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 0 auto;
+}
+.intro_container, .education_container {
+  align-self: center;
+  max-width: 1200px;
+  min-height: 100px;
+}
 </style>
